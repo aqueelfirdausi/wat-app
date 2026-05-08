12ecec6fc6c466e736d1d0ae5fa9af2f552045f6
Aqueel Ahmed Firdausi <aqueelfirdausi@gmail.com>
2026-05-09 03:27:52 +0500

    Enhance APK download button with Android icon and bold styling

--- components/homepage-client.tsx
+++ components/homepage-client.tsx
@@ -543,7 +543,10 @@ export function HomepageClient() {
               rel="noopener noreferrer"
               onClick={dismissInstallHint}
             >
-              Download APK
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" fill="currentColor" aria-hidden="true">
+                <path d="M6 18c0 .55.45 1 1 1h1v3.5c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5V19h2v3.5c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5V19h1c.55 0 1-.45 1-1V8H6v10zM3.5 8C2.67 8 2 8.67 2 9.5v7c0 .83.67 1.5 1.5 1.5S5 17.33 5 16.5v-7C5 8.67 4.33 8 3.5 8zm17 0c-.83 0-1.5.67-1.5 1.5v7c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5v-7c0-.83-.67-1.5-1.5-1.5zm-4.97-5.84 1.3-1.3c.2-.2.2-.51 0-.71-.2-.2-.51-.2-.71 0l-1.48 1.48A5.84 5.84 0 0 0 12 1.5c-.69 0-1.35.14-1.95.39L8.57.41c-.2-.2-.51-.2-.71 0-.2.2-.2.51 0 .71l1.29 1.3A5.969 5.969 0 0 0 6 8h12c0-2.01-.99-3.79-2.47-4.84zM10 5H9V4h1v1zm5 0h-1V4h1v1z" />
+              </svg>
+              <strong>Download APK</strong>
             </a>
           </div>
         </section>
